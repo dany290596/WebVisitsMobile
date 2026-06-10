9c6dd265eb15fd3cdadd9bd02382d1552440cffe
Daniel Benito Rosales <danielbr.estatus@gmail.com>
2026-06-10 16:30:31 -0600

    Ajustes.

--- WebVisitsMobile.Models/HID/CredencialHID/CredencialHIDRespDTO.cs
+++ WebVisitsMobile.Models/HID/CredencialHID/CredencialHIDRespDTO.cs
@@ -25,20 +25,12 @@ namespace WebVisitsMobile.Models.HID.CredencialHID
         public string StatusDescripcion => Status switch
         {
             // Legacy
-            1 => "Emitida",
-            2 => "Activa",
-            3 => "Suspendida",
-            4 => "Revocada",
+            1 => "Creada",
+            2 => "Pendiente",
+            3 => "Asignada",
+            4 => "Suspendida",
             5 => "Expirada",
-            6 => "Eliminada",
-
-            // HID Origo
-            30 => "Reservada",
-            31 => "Emitida",
-            32 => "Revocando",
-            33 => "Revocada",
-            34 => "Desvinculada",
-            35 => "Error de creación",
+            7 => "Eliminada",
 
             null => "Sin estado",
             _ => "Desconocido"
@@ -55,15 +47,7 @@ namespace WebVisitsMobile.Models.HID.CredencialHID
             3 => "#FFA500", // Naranja - Suspendida
             4 => "#FF0000", // Rojo - Revocada
             5 => "#8B4513", // Café - Expirada
-            6 => "#696969", // Gris oscuro - Eliminada
-
-            // HID Origo
-            30 => "#1E90FF", // Azul - Reservada
-            31 => "#32CD32", // Verde - Emitida
-            32 => "#FFA500", // Naranja - Revocando
-            33 => "#FF0000", // Rojo - Revocada
-            34 => "#9370DB", // Morado - Desvinculada
-            35 => "#DC143C", // Rojo intenso - Error de creación
+            7 => "#696969", // Gris oscuro - Eliminada
 
             null => "#2F2F2F", // Gris - Sin estado
             _ => "#2F2F2F"     // Gris - Desconocido
